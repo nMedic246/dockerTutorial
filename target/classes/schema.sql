325bba5386a98c87c508cfd70774c47d54478aaa
nmedic <nika.medic@true-north.hr>
2025-07-20 11:32:15 +0200

    cleanup

--- target/classes/schema.sql
+++
@@ -1,6 +0,0 @@
-CREATE TABLE tutorials (
-                           id SERIAL PRIMARY KEY,
-                           title VARCHAR(100),
-                           description TEXT,
-                           published BOOLEAN
-);
